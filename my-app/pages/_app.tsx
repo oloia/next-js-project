e0471b938f81e15e148a05b3e137ae661126d3f6
olga_lihaia <olga.l@usenergysolutions.us>
2021-11-17 19:20:37 +0200

    Add new font for porject

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import {AppProps} from "next/dist/shared/lib/router/router";
+import Head from "next/head";
+import React from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <>
+    <Head>
+      <title>My top is the best of the best top</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+                rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
